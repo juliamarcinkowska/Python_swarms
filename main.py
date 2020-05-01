38e8047d5d9da86a64aee1e380cf7bbab044f56b
Julia Marcinkowska <juliamar2205@gmail.com>
2020-05-01 18:35:38 +0100

    Algorithm not working (strange plots)

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import math
+from operator import attrgetter
 from random import random
+import matplotlib.pyplot as plt
 
 from Particle import Particle
 
@@ -6,7 +9,11 @@ from Particle import Particle
 def calculate_value_f(p):
     x1, x2, x3, x4 = p.vector_X
     f_value = 0.6224 * x1 * x3 * x4 + 1.7781 * x2 * x3 ** 2 + 3.1661 * x1 ** 2 * x4 + 19.84 * x1 ** 2 * x3
-    return f_value
+    if 0.0193 * x3 - x1 > 0 or 0.00954 * x3 - x2 > 0 \
+            or -math.pi * x3 ** 2 * x4 - 4 / 3 * math.pi * x3 ** 3 + 129600 > 0 or -x4 - 240 > 0:
+        return f_value * 10000
+    else:
+        return f_value
 
 
 def update_values(p, leader):
@@ -19,16 +26,23 @@ def update_values(p, leader):
 
 def main():
     particle_no = 30
+    iter_no = 10000
+    leader_values = []
     particles = [Particle() for i in range(particle_no)]
-    for p in particles:
-        temp_val = calculate_value_f(p)
-        if p.f_value is None:
-            p.f_value = temp_val
-        if temp_val < p.f_value:
-            p.f_value = temp_val
-            p.vector_P = p.vector_X
-    for p in particles:
-        update_values(p, )
+    for i in range(iter_no):
+        for p in particles:
+            temp_val = calculate_value_f(p)
+            if p.f_value is None:
+                p.f_value = temp_val
+            if temp_val < p.f_value:
+                p.f_value = temp_val
+                p.vector_P = p.vector_X
+        leader = min(particles, key=attrgetter('f_value'))
+        leader_values.append(leader.f_value)
+        for p in particles:
+            update_values(p, leader)
+    plt.plot(leader_values)
+    plt.show()
 
 
 if __name__ == "__main__":
