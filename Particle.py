8f70223bb73295e11b14f6657727f8b1a3bf2519
Weronika Górecka <216766@deu.p.lodz.pl>
2020-05-01 19:31:19 +0100

    Check constraints function

--- Particle.py
+++ Particle.py
@@ -9,7 +9,7 @@ class Particle:
         x1, x2, x3, x4 = np.array([random.randint(1, 99), random.randint(1, 99),
                   random.uniform(10.0, 200.0), random.uniform(10.0, 200.0)])
         while 0.0193 * x3 - x1 > 0 or 0.00954 * x3 - x2 > 0 \
-                or -math.pi * x3 ** 2 * x4 - 4 / 3 * math.pi * x3 ** 3 + 129600 > 0 or -x4 - 240 > 0:
+                or -math.pi * x3 ** 2 * x4 - 4 / 3 * math.pi * x3 ** 3 + 1296000 > 0 or -x4 - 240 > 0:
             x1 = random.randint(1, 99)
             x2 = random.randint(1, 99)
             x3 = random.uniform(10.0, 200.0)
--- main.py
+++ main.py
@@ -6,11 +6,23 @@ import matplotlib.pyplot as plt
 from Particle import Particle
 
 
-def calculate_value_f(p):
+def check_constraints(p):
     x1, x2, x3, x4 = p.vector_X
+    if 0.0193 * x3 - x1 <= 0 and 0.00954 * x3 - x2 <= 0 \
+            and -math.pi * x3 ** 2 * x4 - 4 / 3 * math.pi * x3 ** 3 + 1296000 <= 0 \
+            and -x4 - 240 <= 0 and 1 <= x1 <= 99 and 1 <= x2 <= 99 and 10.00 <= x3 <= 200.00 and 10.00 <= x4 <= 200.00:
+        return True
+    else:
+        return False
+
+
+def calculate_value_f(p):
+    p.vector_X[0] = int(p.vector_X[0])
+    p.vector_X[1] = int(p.vector_X[1])
+    x1, x2, x3, x4 = p.vector_X
+    # print(x1, x2, x3, x4)
     f_value = 0.6224 * x1 * x3 * x4 + 1.7781 * x2 * x3 ** 2 + 3.1661 * x1 ** 2 * x4 + 19.84 * x1 ** 2 * x3
-    if 0.0193 * x3 - x1 > 0 or 0.00954 * x3 - x2 > 0 \
-            or -math.pi * x3 ** 2 * x4 - 4 / 3 * math.pi * x3 ** 3 + 129600 > 0 or -x4 - 240 > 0:
+    if not check_constraints(p):
         return f_value * 10000
     else:
         return f_value
@@ -18,9 +30,9 @@ def calculate_value_f(p):
 
 def update_values(p, leader):
     w, c1, c2 = (0.5 * random()) / 2.0, 1.5 * random(), 1.5 * random()
-    v, x, p, pL = p.vector_V, p.vector_X, p.vector_P, leader.vector_P
+    v, x, p, pl = p.vector_V, p.vector_X, p.vector_P, leader.vector_P
     for i in range(4):
-        v[i] = w * v[i] + c1 * (p[i] - x[i]) + c2 * (pL[i] - x[i])
+        v[i] = w * v[i] + c1 * (p[i] - x[i]) + c2 * (pl[i] - x[i])
         x[i] += v[i]
 
 
@@ -43,6 +55,8 @@ def main():
             update_values(p, leader)
     plt.plot(leader_values)
     plt.show()
+    leader = min(particles, key=attrgetter('f_value'))
+    print(leader_values[-1], ' ', leader.vector_P)
 
 
 if __name__ == "__main__":
